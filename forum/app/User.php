c8edd12e8af8ce471d29d562abeaf205300bf738
Tehmeer Ali Paryani <freelanceali786@gmail.com>
2019-07-28 12:07:43 +0500

    building model

--- forum/app/User.php
+++ forum/app/User.php
@@ -26,4 +26,12 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+
+    public function discussions() {
+
+
+        return $this->hasMany('App\Discussion');
+
+    }
 }
